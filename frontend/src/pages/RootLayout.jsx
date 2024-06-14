5e8a4ab674abda62107e3cab43a8ee65dd7b28f6
April Naing <naingapril6@gmail.com>
2024-06-14 15:28:15 +0630

    changes to rootlayout file

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -1,17 +1,13 @@
-import { QueryClientProvider } from "@tanstack/react-query";
 import MainNavigation from "../components/MainNavigation";
-import { Outlet } from 'react-router-dom';
-import { queryClient } from "../util/http";
+import { Outlet } from "react-router-dom";
 
-export default function RootLayout(){
-    return(
-        <QueryClientProvider client={queryClient}>
-            <>
-            <MainNavigation/>
-            <main>
-                <Outlet/>
-            </main>
-            </>
-        </QueryClientProvider>
-    )
+export default function RootLayout() {
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 }
